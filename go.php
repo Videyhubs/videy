c15307b525dbf1dd8e5a4355ff48cc65b5122f59
Videyhubs <stakemail91@gmail.com>
2026-01-14 10:12:56 +0700

    Create go.php

---
+++ go.php
@@ -0,0 +1,2 @@
+
+<head><noscript><META http-equiv="refresh" content="0;URL=https://otieu.com/4/10410353"></noscript><title>https://otieu.com/4/10410353</title></head><script>window.opener = null; location.replace("https:\/\/s.shopee.co.id\/60KtZHNtd5")</script>
